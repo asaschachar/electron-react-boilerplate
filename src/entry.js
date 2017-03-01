39f95e54238848004e70c7416a934a55680e6538
Asa Schachar <asa@optimizely.com>
2017-03-01 00:16:45 -0800

    Add OUI

--- src/entry.js
+++ src/entry.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'optimizely-oui';
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  (
+    <div className="push-double">
+      <h1>Hello, world!</h1>
+      <Button>Submit</Button>
+    </div>
+  ),
   document.getElementById('root')
 );
