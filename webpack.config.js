c7ade93b587433e36ecb3bb266b9a03f313ac6fb
Asa Schachar <asa@optimizely.com>
2017-02-28 22:56:39 -0800

    Add boilerplate electron code

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: "./src/entry.js",
+  output: {
+    path: __dirname,
+    filename: "dist/bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+    ]
+  }
+};
