277345abb7ff3e521e52f9bc5b1af3656c854b51
Asa Schachar <asa@optimizely.com>
2021-05-12 16:19:49 -0700

    Remove oui

--- src/entry.js
+++ src/entry.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from 'optimizely-oui';
 
 ReactDOM.render(
   (
     <div className="push-double">
       <h1>Hello, world!</h1>
-      <Button>Submit</Button>
+      <button>Submit</button>
     </div>
   ),
   document.getElementById('root')
